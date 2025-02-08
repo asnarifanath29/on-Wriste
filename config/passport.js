98a7a26a45bcbdf10b0861924366062eaed8560a
asna rifanath <asnarifanath@gmail.com>
2025-02-08 18:30:11 +0530

    googlelogin

--- config/passport.js
+++ config/passport.js
@@ -8,7 +8,7 @@ const env=require("dotenv").config();
 passport.use(new GoogleStrategy({
     clientID:process.env.GOOGLE_CLIENT_ID,
     clientSecret:process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL:'https://onwriste.shop//auth/google/callback'
+    callbackURL:'https://onwriste.shop/auth/google/callback'
 },
 async(accessToken,refreshToken,profile,done)=>{
     try {
